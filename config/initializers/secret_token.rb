561edce6333dd405a0a36f4f6abfd4f14d924518
Jan Gloser <jan.gloser@centrum.cz>
2011-11-26 19:44:02 +0100

    Created a rails project to serve the files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlexPlayground::Application.config.secret_token = '36b77d220d6366ecf775b46e26841df74ecda8b11b214c710e920a9a218f8e9f0b143cc571428e0d41f744dad93b78647c49ff7fa8e027ca0b7541285bddde32'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FlexPlayground::Application.config.session_store :cookie_store, :key => '_flex-playground_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FlexPlayground::Application.config.session_store :active_record_store
